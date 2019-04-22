eaab8fb512594b9bcc886d396cf26872c31aaaf6
Alberto A <alberto.arpini@fmc-ag.com>
2019-04-22 21:07:24 +0200

    added config pre load

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import environment from './environments/environment.json';
 
-if (environment.production) {
-  enableProdMode();
-}
+(async () => {
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  // console.time()
+  const response = await fetch('./environments/environment.json');
+  const json = await response.json();
+  Object.entries(json).forEach(([key, value]) => {
+    environment[key] = value;
+  });
+  // console.timeEnd()
+
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+
+  if (environment.production) {
+    enableProdMode();
+  }
+
+})();
